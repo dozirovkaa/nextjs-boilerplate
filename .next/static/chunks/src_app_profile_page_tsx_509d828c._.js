7c41ddedc9db6e9c68356229755571994f94090a
dozirovkaa <egorovchin11@gmail.com>
2025-05-19 19:54:43 +0500

    Add .next/static/chunks/src_app_profile_page_tsx_509d828c._.js 1747666482661

---
+++ .next/static/chunks/src_app_profile_page_tsx_509d828c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ee05adbb._.js",
+  "static/chunks/src_app_profile_page_tsx_e9387700._.js"
+],
+    source: "dynamic"
+});
